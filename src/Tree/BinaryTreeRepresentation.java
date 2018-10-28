f9a2021f8e0ad0cd009c7e6c0a1baf6639af8e1a
saiful00000 <>
2018-10-28 21:25:55 +0600

    Binry tree implemented

--- src/Tree/BinaryTreeRepresentation.java
+++ src/Tree/BinaryTreeRepresentation.java
@@ -1,7 +1,7 @@
 package Tree;
 
 public class BinaryTreeRepresentation {
-    class Node{
+    static class Node{
         int value;
         Node left, right;
 
@@ -10,4 +10,27 @@ public class BinaryTreeRepresentation {
             left = right = null;
         }
     }
+
+    static class BinaryTree {
+        Node root;
+
+        BinaryTree() { }
+
+        BinaryTree(int value) {
+            root = new Node(value);
+        }
+
+        public static void main(String[] args) {
+            BinaryTree tree = new BinaryTree();
+
+            tree.root = new Node(100);
+            tree.root.left = new Node(200);
+            tree.root.right = new Node(300);
+            tree.root.left.left = new Node(400);
+            tree.root.left.right = new Node(500);
+            tree.root.right.left = new Node(600);
+            tree.root.right.right = new Node(700);
+        }
+    }
+
 }
