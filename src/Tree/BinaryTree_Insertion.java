958913c1df7bdf266b2475eeeddedb553034ccdf
saiful00000 <>
2018-10-28 23:55:28 +0600

    Node class done

---
+++ src/Tree/BinaryTree_Insertion.java
@@ -0,0 +1,16 @@
+package Tree;
+
+public class BinaryTree_Insertion {
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            left = right = null;
+        }
+    }
+
+}
