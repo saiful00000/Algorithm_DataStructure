ee20622e5053eb4d3eb31330a56bb3d2726906b7
saiful00000 <>
2018-11-17 21:19:11 +0600

    min value of BST recursive method

--- src/Tree/BinarySearchTree/MinValueOfBST.java
+++ src/Tree/BinarySearchTree/MinValueOfBST.java
@@ -1,8 +1,7 @@
 package Tree.BinarySearchTree;
 
-public class BST_Search_A_Given_Key {
-    //class for constructing the structure of a node
-    static class Node {
+public class MinValueOfBST {
+    static class Node{
         int data;
         Node left, right;
 
@@ -11,27 +10,8 @@ public class BST_Search_A_Given_Key {
         }
     }
 
-    //class for implement Binary tree and perform search operation
     static class BST {
 
-        // method for cheack if the given key is exist i the tree
-        static void searchTheKey(Node root, int key) {
-            if (root == null) {
-                System.out.println("The given key is not found.");
-                return;
-            }
-            if (root.data == key) {
-                System.out.println("The given key is exist in the Tree");
-                return;
-            }
-
-            if (key < root.data)
-                searchTheKey(root.left, key);
-            else
-                searchTheKey(root.right, key);
-        }
-
-        // method for insert value in binary Tree
         static Node insert(Node root, int data) {
             if (root == null)
                 return new Node(data);
@@ -45,22 +25,19 @@ public class BST_Search_A_Given_Key {
         }
 
         static int minValue(Node root) {
+            if (root == null) return 0;
             if (root.left == null)
                 return root.data;
             return minValue(root.left);
         }
 
-        //driver method of the class or the main method
         public static void main(String[] args) {
             Node root = null;
             int[] array = {15, 10, 20, 8, 12, 16, 25};
             for (int data: array) {
                 root = insert(root, data);
             }
-
-            searchTheKey(root,20);
             System.out.println(minValue(root));
         }
     }
-
 }
